5b4ecf7a57ee2a2c2e64e9742af312fb65a69048
powerboat9 <powerboat9@users.noreply.github.com>
2016-05-06 07:03:47 -0400

    Update autopilot.cs

--- autopilot.cs
+++ autopilot.cs
@@ -1,3 +1,9 @@
+List<IMyThrust> getThrusters()
+    List<IMyThrust> thrusters = new List<IMyTerminalBlock>();
+    GridTerminalSystem.getBlocksOfType<IMyThrust>(thrusters);
+    return thrusters;
+}
+
 void Main() {
-    --
+    List<IM
 }
